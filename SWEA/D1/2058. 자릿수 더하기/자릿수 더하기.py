a2df1f31a373881a093f1d22d2ed94ce9bc762e0
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-10 22:08:27 +0900

    [D1] Title: 자릿수 더하기, Time: 93 ms, Memory: 44,728 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,14 +1,19 @@
-T = int(input())
-sum = 0
-listT = []
-i = 0
-while True:
-    if T == 0:
-        break
-    else: 
-        listT.append(T % 10)
-        T = T//10
-        i += 1
-for j in range(len(listT)):
-    sum += listT[j]
-print(sum)
+# int형으로 변환
+T = list(map(int, input()))
+print(sum(T))
+
+# map이랑 sum 함수 없이 구하기
+# T = int(input())
+# sum = 0
+# listT = []
+# i = 0
+# while True:
+#     if T == 0:
+#         break
+#     else: 
+#         listT.append(T % 10)
+#         T = T//10
+#         i += 1
+# for j in range(len(listT)):
+#     sum += listT[j]
+# print(sum)
