a6734b8fd46bf4a5fdfcbb6375b57d3ed7157e52
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-10 19:13:09 +0900

    [D1] Title: 홀수만 더하기, Time: 96 ms, Memory: 44,732 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
@@ -1,9 +1,9 @@
 t = int(input())
- 
-for test_case in range(1,t+1):
-    li = map(int, input().split())
+for test_case in range(1, t + 1):
+    li = list(map(int, input().split()))
     answer = 0
     for i in li:
-        if i%2!=0:
+        if i % 2 == 1:  # 홀수인 경우만 더함
             answer += i
-    print("#"+str(test_case),str(answer))
+    # answer를 출력하는 부분 수정
+    print(f"#{test_case} {answer}")
