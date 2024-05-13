8adb69a84efaedc8d50f331e5d3637ef554821df
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-13 20:34:59 +0900

    [D1] Title: 대각선 출력하기, Time: 95 ms, Memory: 44,708 KB -BaekjoonHub

--- SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
+++ SWEA/D1/2027. 대각선 출력하기/대각선 출력하기.py
@@ -1,5 +1,12 @@
-default = ['+','+','+','+','+']
+# default = ['+','+','+','+','+']
+# for i in range(5):
+#     default[i] = '#'
+#     print(''.join(default))
+#     default = ['+','+','+','+','+']
 for i in range(5):
-    default[i] = '#'
-    print(''.join(default))
-    default = ['+','+','+','+','+']
+    for j in range(5):
+        if i == j:
+            print("#", end = '')
+        else:
+            print("+", end = '') 
+    print()
