b2629725536c0c0bf19653387d8535c8cff3455c
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-13 22:42:34 +0900

    [D1] Title: 아주 간단한 계산기, Time: 92 ms, Memory: 43,144 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -0,0 +1,15 @@
+a, b = tuple(map(int, input().split()))
+
+def sum(a, b):
+    return a+b
+def sub(a, b):
+    return a-b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return a // b
+
+print(sum(a,b))
+print(sub(a,b))
+print(mul(a,b))
+print(div(a,b))
