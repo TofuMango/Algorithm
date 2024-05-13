65dd941b40ac6fa39d1465bffa5929df24ca2ce4
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-13 23:37:20 +0900

    [D1] Title: 더블더블, Time: 92 ms, Memory: 44,968 KB -BaekjoonHub

--- SWEA/D1/2019. 더블더블/더블더블.py
+++ SWEA/D1/2019. 더블더블/더블더블.py
@@ -1,6 +1,10 @@
-t = int(input())
-ans = 1
-print(ans, end=' ')
-for i in range(1,t+1):
-    ans *= 2
-    print(ans, end=' ')
+# t = int(input())
+# ans = 1
+# print(ans, end=' ')
+# for i in range(1,t+1):
+#     ans *= 2
+#     print(ans, end=' ')
+
+n = int(input())
+for i in range(n+1):
+    print(2**i, end = ' ')
