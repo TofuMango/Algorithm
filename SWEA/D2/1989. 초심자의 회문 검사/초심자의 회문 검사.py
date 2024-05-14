ed8f44930be2db1abbe2622e27ec8c83a3013137
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-15 02:08:23 +0900

    [D2] Title: 초심자의 회문 검사, Time: 98 ms, Memory: 44,204 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -10,3 +10,11 @@ for test_case in range(1, t+1):
         print(f"#{test_case} 1")
     else:
         print(f"#{test_case} 0")
+
+# T = int(input())
+# for test_case in range(1, T + 1):
+#     word = input()
+#     if word == word[::-1]:
+#         print(f"#{test_case} 1")
+#     else:
+#         print(f"#{test_case} 0")
