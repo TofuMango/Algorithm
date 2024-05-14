e97a3f7d00953551291cc4289e63f3c0b4174ffb
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-15 03:08:22 +0900

    [D2] Title: 두 개의 숫자열, Time: 94 ms, Memory: 45,248 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,30 +1,55 @@
 t = int(input())
-for test_case in range(1, t+1):
-    n, m = tuple(map(int, input().split()))
+for test_case in range(1, t + 1):
+    n, m = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    # n과 m 공간 크기 비교
+    
+    # 길이가 더 짧은 배열과 긴 배열을 구분
     if n > m:
-        maxSpace = n
-        minSpace = m
-        longer, shorter = A, B
+        short, long = B, A
+        short_len, long_len = m, n
     else:
-        maxSpace = m
-        minSpace = n
-        longer, shorter = B, A
-    cnt = 0
-    maxSum = 0
-    tmp = 0
-    # 최댓값 구하기
-    while True:
-        tmp = 0
-        for i in range(minSpace):
-            # 짧은배열은 고정시키고 긴배열만 이동시키기
-            tmp += longer[i+cnt] * shorter[i]
-        maxSum = max(maxSum, tmp)
-        # 범위가 넘어가면 break
-        if cnt >= maxSpace-minSpace:
-            print(f"#{test_case} {maxSum}")
-            break
-        else:
-            cnt += 1
+        short, long = A, B
+        short_len, long_len = n, m
+    
+    max_sum = -1e9  # 가능한 최소값으로 초기화
+    
+    #최댓값 구하기
+    for start in range(long_len - short_len + 1):
+        current_sum = 0
+        for i in range(short_len):
+            current_sum += short[i] * long[start + i]
+        max_sum = max(max_sum, current_sum)
+    
+    print(f"#{test_case} {max_sum}")
+
+# t = int(input())
+# for test_case in range(1, t+1):
+#     n, m = tuple(map(int, input().split()))
+#     A = list(map(int, input().split()))
+#     B = list(map(int, input().split()))
+#     # n과 m 공간 크기 비교
+#     if n > m:
+#         maxSpace = n
+#         minSpace = m
+#         longer, shorter = A, B
+#     else:
+#         maxSpace = m
+#         minSpace = n
+#         longer, shorter = B, A
+#     cnt = 0
+#     maxSum = 0
+#     tmp = 0
+#     # 최댓값 구하기
+#     while True:
+#         tmp = 0
+#         for i in range(minSpace):
+#             # 짧은배열은 고정시키고 긴배열만 이동시키기
+#             tmp += longer[i+cnt] * shorter[i]
+#         maxSum = max(maxSum, tmp)
+#         # 범위가 넘어가면 break
+#         if cnt >= maxSpace-minSpace:
+#             print(f"#{test_case} {maxSum}")
+#             break
+#         else:
+#             cnt += 1
