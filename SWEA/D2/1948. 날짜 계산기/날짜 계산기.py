3745615f2c60de60c5ca68671609b427b1d9e929
Yeji Lee <96580357+TofuMango@users.noreply.github.com>
2024-05-16 16:42:31 +0900

    [D2] Title: 날짜 계산기, Time: 112 ms, Memory: 45,228 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -5,13 +5,25 @@ for test_case in range(1, t+1):
     cnt = 0
     # 날짜 계산 로직
     while True:
+        if m1 == m2 and d1 == d2:
+            cnt += 1
+            print(f"#{test_case} {cnt}")
+            break
         if d1 <= day[m1-1]:
             cnt += 1
             d1 += 1
         else:
             m1 += 1
             d1 = 1
-        if m1 == m2 and d1 == d2:
-            cnt+=1
-            print(f"#{test_case} {cnt}")
-            break
+# month = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+# T = int(input())
+# for test_case in range(1, T + 1):
+#     change = 1
+#     m1, d1, m2, d2 = list(map(int, input().split()))
+#     if m1 == m2:
+#         change += d2 - d1
+#     else:
+#         for i in range(m1, m2):
+#             change += month[i]
+#         change += d2 - d1
+#     print(f"#{test_case} {change}")
